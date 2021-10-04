a1e1173059b924bf98c0c376a028ee5c510fd593
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-04 17:22:34 -0400

    updated the Content script with conditional routing

---
+++ src/components/Content.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Porfolio";
+
+export default function Content() {
+    const [currentPage, setCurrentPage] = useState("About");
+
+    const renderPage = () => {
+        if (currentPage === "Contact") {
+            return <Contact />;
+        }
+        if (currentPage === "Portfolio") {
+            return <Portfolio />;
+        }
+        return <About />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+}
