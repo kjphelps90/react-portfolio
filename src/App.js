10d82c567b4f27e4bbb6a146cea5535e5b691969
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-04 17:01:34 -0400

    added component pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import About from "./components/About";
+import Contact from "./components/Content";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Content />
+        </div>
+    )
+}
