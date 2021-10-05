12fd757a96e1f6221253803e264a3a3c0008b22c
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-04 20:03:13 -0400

    updated NavTabs formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Header() {
     return (
-        <div className="container row align-items-center my-5">
+        <div className="container align-items-center my-5">
             <div className="col-lg-3">
                <h1>Kevin Phelps</h1>
             </div>
             <div className="col-lg-3">
                 <h5>(734)-972-0619</h5>
                 <h5>kjphelps90@gmail.com</h5>
-                <h5>Github <a href="https://github.com/kjphelps90">kjphelps90</a></h5>
+                <h5>Github: <a href="https://github.com/kjphelps90">kjphelps90</a></h5>
             </div>
             <div className="col-lg-6">
             </div>
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,33 +2,36 @@ import React from "react";
 
 export default function NavTabs({ currentPage, handlePageChange}) {
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
+        <div className="container justify-content-center">
+        <ul className="justify-content-center nav nav-tabs">
+            <li className="nav-item mx-4 px-2 fs-5 fw-bold">
             <a
                 href="#about"
                 onClick={() => handlePageChange("About")}
                 className={currentPage === "About" ? "nav-link active" : "nav-link"}
             >About Me</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-4 px-2 fs-5 fw-bold">
             <a
                 href="#portfolio"
                 onClick={() => handlePageChange("Portfolio")}
                 className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}
             >Portfolio</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item mx-4 px-2 fs-5 fw-bold">
             <a
                 href="#contact"
                 onClick={() => handlePageChange("Contact")}
                 className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
             >Contact</a>
             </li>
-            {/* <li className="nav-item">
+            <li className="nav-item mx-4 px-2 fs-5 fw-bold">
             <a
-                href="../assets/docs/Resume_KevinPhelps" download
+                className="nav-link"
+                href=".\assets\docs\Resume_KevinPhelps.pdf" download
             >Resume</a>
-            </li> */}
+            </li>
         </ul>
+    </div>
     )
 }
