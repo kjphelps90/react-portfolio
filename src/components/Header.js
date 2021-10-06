ed0aee95dd33ba140e506cec3bb52dcf90106535
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-06 19:14:31 -0400

    added image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import React from "react";
+import Phelps from "../assets/images/phelps.jpg";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../assets/css/style.css";
 
 export default function Header() {
     return (
-        <div className="container align-items-center my-5">
+        <div className="container my-5">
+            <div className="row">
             <div className="col-lg-3">
-               <h1>Kevin Phelps</h1>
+                <img className="self" src={Phelps}/> 
             </div>
             <div className="col-lg-3">
+            <h1>Kevin Phelps</h1>
                 <h5>(734)-972-0619</h5>
                 <h5>kjphelps90@gmail.com</h5>
                 <h5>Github: <a href="https://github.com/kjphelps90">kjphelps90</a></h5>
             </div>
             <div className="col-lg-6">
+            </div>
             </div>
         </div>
     )
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,37 @@
 import React from "react";
 
+
 export default function Contact() {
     return (
         <div className="container my-5">
-            <h1>Contact</h1>
-            <li><span>Phone: </span>   (734)-972-0619</li>
-            <li><span>Email: </span> <a href="tomail:kjphelps90@gmail.com">kjphelps90@gmail.com</a></li>
-            <li><span>Address: </span> 309 Hillcrest Dr, Saline, Michigan 48176</li>
-            <li><span>LinkedIn: </span> <a href="https://www.linkedin.com/in/kevin-phelps-30359b168/">https://www.linkedin.com/in/kevin-phelps-30359b168/</a></li>
-            <li><span>Github: </span> <a href="https://github.com/kjphelps90">https://github.com/kjphelps90</a></li>
+        <form className="form">
+        <div className = "my-4">
+        <input
+          name="userName"
+          type="text"
+          placeholder="name"
+        />
         </div>
+        <div className="my-4">
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+        />
+        </div>
+        <div className="my-4">
+        <input
+          className="textarea"
+          name="textarea"
+          type="textarea"
+          placeholder="Please enter your question"
+        />
+        </div>
+        <div>
+            <button type="button">Submit</button>
+        </div>
+        </form>
+        </div>
+        
     );
 }
