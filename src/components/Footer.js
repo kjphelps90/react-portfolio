bfa0dde032ea3e1fce9fc82d652aad7a1fc1cad9
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-10-06 19:32:56 -0400

    added links to bottom and updated contact form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className="container fixed-bottom">
-            <h2>footer</h2>
+        <div className="container d-flex justify-content-center orange">
+            <a className="mx-5 mb-5" href="https://www.linkedin.com/in/kevin-phelps-30359b168/">LinkedIn</a>
+            <a href="https://github.com/kjphelps90/">GitHub</a>
         </div>
     );
 }
